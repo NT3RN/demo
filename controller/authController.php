b935aee46e253aa9dec7d66c321def0c2a180147
nafim <nafim@gmail.com>
2025-09-27 00:13:02 +0600

    login page design update

--- controller/authController.php
+++ controller/authController.php
@@ -1,52 +1,48 @@
 <?php
-    require_once ("userController.php");
+require_once("userController.php");
 
-    $emailErr ="";
-    $passErr ="";
-    $hasErr = false;
-    if(($_SERVER["REQUEST_METHOD"]=="POST") && (isset($_POST["submit"])))
-    {
-        
-        $email = trim($_POST["email"]);
-        $pass = trim($_POST["password"]);
+$emailErr = "";
+$passErr = "";
+$hasErr = false;
 
-        if(empty($email)){
-            $emailErr = "Email can't be empty";
-            $hasErr = true;
-        }
+if (($_SERVER["REQUEST_METHOD"] == "POST") && (isset($_POST["submit"]))) {
+    
+    $email = trim($_POST["email"]);
+    $pass = trim($_POST["password"]);
 
-        if (empty($pass)) {
-            $passErr = "Password can't be empty";
-            $hasErr = true;
-        }
-
-        if($hasErr){
-            header("Location: ../view/login.php?emailErr =$emailErr&passErr=$passErr");
-            exit();
-        }
-        else
-        {
-            $users = searchUser($_POST["email"], $_POST["password"]);
-            if(!$users){
-                header("Location:../view/login.php?invalidUser = Invalid User");
-            }
-            else
-            {
-                session_start();
-                $_SESSION["user_id"] = $users['user_id'];
-                $_SESSION["email"]=$users["email"];
-                $_SESSION["role"]=$users["role"];
-                if($users["role"]=="Admin"){
-                    header("Location:../view/admin/adminDashboard.php");
-                }
-                elseif($users["role"]=="Manager"){
-                    header("Location: ../view/manager/managerDashboard.php");
-                }
-                else{
-                    header("Location:../view/customer/customerDashboard.php");
-                }
-            }
-        }
+    if (empty($email)) {
+        $emailErr = "Email can't be empty";
+        $hasErr = true;
     }
 
+    if (empty($pass)) {
+        $passErr = "Password can't be empty";
+        $hasErr = true;
+    }
+
+    if ($hasErr) {
+        header("Location: ../view/login.php?emailErr=" . $emailErr . "&passErr=" . $passErr);
+        exit();
+    } else {
+        $users = searchUser($_POST["email"], $_POST["password"]);
+        if (!$users) {
+            header("Location: ../view/login.php?loginErr=Invalid email or password");
+            exit();
+        } else {
+            session_start();
+            $_SESSION["user_id"] = $users['user_id'];
+            $_SESSION["email"] = $users["email"];
+            $_SESSION["role"] = $users["role"];
+            
+            if ($users["role"] == "Admin") {
+                header("Location: ../view/admin/adminDashboard.php");
+            } elseif ($users["role"] == "Manager") {
+                header("Location: ../view/manager/managerDashboard.php");
+            } else {
+                header("Location: ../view/customer/customerDashboard.php");
+            }
+            exit();
+        }
+    }
+}
 ?>
--- view/login.php
+++ view/login.php
@@ -1,54 +1,61 @@
 <!DOCTYPE html>
 <html>
-
 <head>
     <link rel="stylesheet" href="css/login.css">
+    <title>Login - SmartCafe</title>
 </head>
-
 <body>
-    <?php
-
-    ?>
     <div id="loginDiv">
         <form method="post" action="../controller/authController.php">
+            <div class="logo-container">
+                <a href="../index.php">
+                    <img src="../assets/logo.png" alt="SmartCafe Logo" class="logo">
+                </a>
+            </div>
+            
             <h1>Login to Smart Cafe</h1>
+            
             <div>
-                <small class="classDanger" id="loginErr">
-                    
-                </small>
+                <small class="classDanger" id="loginErr" style="color: red;">
                     <?php
                     if (isset($_GET["loginErr"])) {
-                        echo $_GET["loginErr"];
+                        echo htmlspecialchars($_GET["loginErr"]);
+                    }
+                    if (isset($_GET["invalidUser"])) {
+                        echo htmlspecialchars($_GET["invalidUser"]);
                     }
                     ?>
                 </small>
             </div>
+            
             <div class="input-group">
                 <input type="email" name="email" id="email" placeholder="Enter your email" autocomplete="off">
                 <span id="emailErr" style="color: red;">
                     <?php
                     if (isset($_GET["emailErr"])) {
-                        echo $_GET["emailErr"];
+                        echo htmlspecialchars($_GET["emailErr"]);
                     }
                     ?>
-                </span><br>
+                </span>
             </div>
+            
             <div class="input-group">
                 <input type="password" name="password" id="password" placeholder="Password" autocomplete="off">
                 <span id="passErr" style="color: red;">
                     <?php
                     if (isset($_GET["passErr"])) {
-                        echo $_GET["passErr"];
+                        echo htmlspecialchars($_GET["passErr"]);
                     }
                     ?>
-                </span><br>
+                </span>
             </div>
+            
             <input type="submit" name="submit" value="Login">
+            
             <div class="forgetpass">
-                <p><a href="register.php">Can't access your account? </a></p>
+                <p><a href="register.php">Can't access your account?</a></p>
             </div>
         </form>
-        <script src="js/login.js"></script>
+    </div>
 </body>
-
 </html>
